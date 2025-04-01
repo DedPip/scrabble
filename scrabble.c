f823b112d1cf02ae34ae6db5e1784e1f70ff3942
DedPip <125252491+DedPip@users.noreply.github.com>
2025-04-01 17:40:30 +0330

    Add demo of count_score func

--- scrabble.c
+++ scrabble.c
@@ -1,5 +1,6 @@
 #include "./libs/cs50.h"
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
@@ -17,20 +18,101 @@ int main(void)
     string point4s = "fhvwy";
     string point5s = "k";
     string point8s = "jx";
-    string ponnt10s = "qz";
+    string point10s = "qz";
         
-    // }
-    // characters with score 1: A, E, I, L, N, O, R, S, T, U,
-
-    // characters with score 2: D, G, 
-
-    // characters with score 3: B, C, M, P,
-
-    // characters with score 4: F, H, V, W, Y,
-
-    // characters with score 5: K
+    string word = "abcdefghijklmnopqrst";
+    int score = 0;
     
-    // chracters with score 8: J,  X,
+    for (int i = 0, word_length = strlen(word); i < word_length; i++)
+    {
 
-    // characters with score 10: Q, Z,
+        int foundScore = 0;
+        for (int j = 0, point1s_length = strlen(point1s); j < point1s_length; j++)
+        {
+            if (word[i] == point1s[j])
+            {
+                score += 1;
+                foundScore = 1;
+                break;
+            }
+        }
+        if (foundScore == 1) {continue;;}
+
+
+        for (int j = 0, point2s_length = strlen(point2s); j < point2s_length; j++)
+        {
+            if (word[i] == point2s[j])
+            {
+                score += 2;
+                foundScore = 1;
+                break;
+            }
+        }
+        if (foundScore == 1) {continue;}
+
+
+        for (int j = 0, point3s_length = strlen(point3s); j < point3s_length; j++)
+        {
+            if (word[i] == point3s[j])
+            {
+                score += 3;
+                foundScore = 1;
+                break;
+            }
+        }
+        if (foundScore == 1) {continue;}
+
+
+        for (int j = 0, point4s_length = strlen(point4s); j < point4s_length; j++)
+        {
+            if (word[i] == point4s[j])
+            {
+                score += 4;
+                foundScore = 1;
+                break;
+            }
+        }
+        if (foundScore == 1) {continue;}
+
+
+        for (int j = 0, point5s_length = strlen(point5s); j < point5s_length; j++)
+        {
+            if (word[i] == point5s[j])
+            {
+                score += 5;
+                foundScore = 1;
+                break;
+            }
+        }
+        if (foundScore == 1) {continue;}
+
+
+        for (int j = 0, point8s_length = strlen(point8s); j < point8s_length; j++)
+        {
+            if (word[i] == point8s[j])
+            {
+                score += 8;
+                foundScore = 1;
+                break;
+            }
+        }
+        if (foundScore == 1) {continue;}
+
+
+        for (int j = 0, point10s_length = strlen(point10s); j < point10s_length; j++)
+        {
+            if (word[i] == point10s[j])
+            {
+                score += 10;
+                foundScore = 1;
+                break;
+            }
+        }
+        if (foundScore == 1) {continue;}
+
+    }
+
+    printf("%i\n", score);
+
+
 }
