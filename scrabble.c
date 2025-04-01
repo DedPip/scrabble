88a161e476d7ae1d397096d037d2989cff88e9cc
DedPip <125252491+DedPip@users.noreply.github.com>
2025-04-01 13:24:16 +0330

    Add variables for number of each score type

--- scrabble.c
+++ scrabble.c
@@ -3,5 +3,24 @@
 
 int main(void)
 {
+    int num_of_1_points = 10;
+    int num_of_2_points = 2;
+    int num_of_3_points = 4;
+    int num_of_4_points = 5;
+    int num_of_5_points = 1;
+    int num_of_8_points = 2;
+    int num_of_10_points = 2;
+    // characters with score 1: A, E, I, L, N, O, R, S, T, U,
+
+    // characters with score 2: D, G, 
+
+    // characters with score 3: B, C, M, P,
+
+    // characters with score 4: F, H, V, W, Y,
+
+    // characters with score 5: K
     
+    // chracters with score 8: J,  X,
+
+    // characters with score 10: Q, Z,
 }
